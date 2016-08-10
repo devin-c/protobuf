eec0d70b108ec333cb810bddc789f311f1c22814
Devin Christensen <quixoten@gmail.com>
2016-08-10 15:22:00 -0600

    Add nats to CONNECTORS

--- lib/protobuf.rb
+++ lib/protobuf.rb
@@ -31,7 +31,7 @@ require 'protobuf/descriptors'
 module Protobuf
 
   # See Protobuf#connector_type documentation.
-  CONNECTORS = [:socket, :zmq].freeze
+  CONNECTORS = [:socket, :zmq, :nats].freeze
 
   # Default is Socket as it has no external dependencies.
   DEFAULT_CONNECTOR = :socket
